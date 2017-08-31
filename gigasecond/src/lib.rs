e5e371813a6c3a2a5136a0a5f253b5f5af28f88d
Prajwal Tuladhar <praj@infynyxx.com>
2017-08-31 21:08:23 +0000

    upgrade chrono lib

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
 extern crate chrono;
 use chrono::*;
 
-pub fn after(dt: DateTime<UTC>) -> DateTime<UTC> {
+pub fn after(dt: DateTime<Utc>) -> DateTime<Utc> {
     dt + Duration::seconds(1_000_000_000)
 }
