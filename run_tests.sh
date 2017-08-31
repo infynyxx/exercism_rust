a19cf2a27d60f28ad8d00e1e660b6fc407ed8591
Prajwal Tuladhar <praj@infynyxx.com>
2017-08-31 21:08:10 +0000

    ignore etl directory

--- run_tests.sh
+++ run_tests.sh
@@ -3,12 +3,15 @@
 cur_dir=$(pwd); 
 for i in $(ls -d */)
 do
+    if [ $i == "etl/" ]; then
+        continue
+    fi
+
     cd ${i%%/}
     cargo test
     exit_code=$?
     cd $cur_dir
-    if [ $exit_code -ne 0 ]
-    then
+    if [ $exit_code -ne 0 ]; then
         echo "test failure for $i"
         exit $exit_code
     fi    
