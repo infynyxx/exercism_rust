c34471b768acf8204d6f6393f264e3ce862b352c
Prajwal Tuladhar <praj@infynyxx.com>
2017-08-31 20:22:37 +0000

    TravisCI integration

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cur_dir=$(pwd); 
+for i in $(ls -d */)
+do
+    cd ${i%%/}
+    cargo test
+    exit_code=$?
+    cd $cur_dir
+    if [ $exit_code -ne 0 ]
+    then
+        echo "test failure for $i"
+        exit $exit_code
+    fi    
+done
